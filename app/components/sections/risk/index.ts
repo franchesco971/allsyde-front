23c7505913cb76b314b649a921374066a5416acb
franchesc971 <franchesco971@gmail.com>
2026-01-30 16:31:55 +0100

    lint

--- app/components/sections/risk/index.ts
+++ app/components/sections/risk/index.ts
@@ -1,4 +1,4 @@
-export { default as RiskHeader } from './riskHeader';
+// export { default as RiskHeader } from './riskHeader';
 // export { default as RiskDashboardTab } from './dashboardTab';
-export { default as RiskReservesTab } from './reservesTab';
+// export { default as RiskReservesTab } from './reservesTab';
 export { default as DutyDetail } from './duty';
